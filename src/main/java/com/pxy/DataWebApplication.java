ab6eab161be1dedf0f6a8383b8985bc5d28567f8
unknown <1416676692@qq.com>
2020-08-28 17:56:37 +0800

    first commit

---
+++ src/main/java/com/pxy/DataWebApplication.java
@@ -0,0 +1,15 @@
+package com.pxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DataWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataWebApplication.class, args);
+        System.out.println("服务启动成功。。。");
+
+}
+
+}
---
+++ src/main/java/com/pxy/controller/indexController.java
@@ -0,0 +1,16 @@
+package com.pxy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class indexController {
+    @RequestMapping("/index")
+    public void index(){
+
+
+
+    }
+
+}
